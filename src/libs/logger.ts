db918fb441ff99ae5b1b1d4be7e1783e04758548
ADMIN\Admin <ducanhmc24@gmail.com>
2025-10-29 10:26:25 +0700

    (Fix): Logger file saving

--- src/libs/logger.ts
+++ src/libs/logger.ts
@@ -18,14 +18,6 @@ type Logger = winston.Logger;
 
 const logDir = path.resolve(process.cwd(), LOGGER_CONSTANTS.BASE_DIR);
 const { combine, timestamp, printf } = winston.format;
-
-const levelColor: Record<ELoggerLevel, typeof textColor> = {
-  [ELoggerLevel.INFO]: textColor.cyanBright.bold,
-  [ELoggerLevel.WARN]: textColor.yellowBright.bold,
-  [ELoggerLevel.ERROR]: textColor.redBright.bold,
-  [ELoggerLevel.DEBUG]: textColor.magentaBright.bold,
-};
-
 export class Log {
   private readonly logger: Logger;
 
@@ -33,52 +25,42 @@ export class Log {
     if (!fs.existsSync(logDir)) fs.mkdirSync(logDir, { recursive: true });
 
     this.logger = winston.createLogger({
+      levels: this.customLevels,
+      level: this.getLevel(ELoggerLevel.ERROR),
       format: combine(timestamp({ format: LOGGER_CONSTANTS.FORMAT_TIMESTAMP }), this.formatConsole),
-      transports: [new winston.transports.Console(), ...this.fileTransports],
+      transports: [new winston.transports.Console()],
     });
   }
 
-  public info<T extends unknown>(data: T, ...meta: string[]) {
-    this.logger.info(serialize(data), { level: ELoggerLevel.INFO }, ...meta);
+  public error<T>(data: T, ...meta: any[]) {
+    this.log(ELoggerLevel.ERROR, data, ...meta);
   }
 
-  public error<T extends unknown>(data: T, ...meta: string[]) {
-    this.logger.error(serialize(data), { level: ELoggerLevel.ERROR }, ...meta);
+  public warn<T>(data: T, ...meta: any[]) {
+    this.log(ELoggerLevel.WARN, data, ...meta);
   }
 
-  public warn<T extends unknown>(data: T, ...meta: string[]) {
-    this.logger.warn(serialize(data), { level: ELoggerLevel.WARN }, ...meta);
+  public info<T>(data: T, ...meta: any[]) {
+    this.log(ELoggerLevel.INFO, data, ...meta);
   }
 
-  public debug<T extends unknown>(data: T, ...meta: string[]) {
-    this.logger.debug(serialize(data), { level: ELoggerLevel.DEBUG }, ...meta);
+  public debug<T>(data: T, ...meta: any[]) {
+    this.log(ELoggerLevel.DEBUG, data, ...meta);
+  }
+
+  private log<T>(level: ELoggerLevel, data: T, ...meta: any[]) {
+    const message = serialize(data);
+
+    this.writeLogFile(level, message, ...meta);
+    this.logger.log(this.getLevel(level), message, ...meta);
   }
 
   private get formatConsole() {
     return printf(({ level, message, timestamp, [Symbol.for('splat')]: splats = [] }) => {
-      let levelTag: string;
-
-      const [customExtras, ...metaSpats]: unknown[] = (splats as unknown[]) ?? [];
-
-      const customLevel = (customExtras as any)?.level ?? level;
-
-      switch (customLevel) {
-        case ELoggerLevel.INFO:
-        case ELoggerLevel.WARN:
-        case ELoggerLevel.ERROR:
-        case ELoggerLevel.DEBUG:
-          levelTag = levelColor[customLevel as ELoggerLevel](
-            boundString(customLevel, LOGGER_CONSTANTS.BOUNDARY_META_CONSOLE)
-          );
-          break;
-
-        default:
-          levelTag = textColor.white(customLevel);
-          break;
-      }
+      const levelTag = this.getLevelColor(level, LOGGER_CONSTANTS.BOUNDARY_META_CONSOLE);
 
       const extras =
-        (metaSpats as unknown[])
+        (splats as unknown[])
           .map?.((v) =>
             textColor.bgMagentaBright.bold(
               boundString(serialize(v), LOGGER_CONSTANTS.BOUNDARY_META_CONSOLE)
@@ -109,41 +91,28 @@ export class Log {
     });
   }
 
-  private get formatFile() {
-    return printf(({ message, timestamp, [Symbol.for('splat')]: splats = [] }) => {
-      const [_customExtras, ...metaSpats]: unknown[] = (splats as unknown[]) ?? [];
+  private formatFile({
+    message,
+    timestamp,
+    [Symbol.for('splat')]: splats = [],
+  }: winston.Logform.TransformableInfo): string {
+    const extras =
+      (splats as unknown[])
+        .map?.((v) => boundString(serialize(v), LOGGER_CONSTANTS.BOUNDARY_META_FILE))
+        .join(LOGGER_CONSTANTS.META_SPLIT) || '';
 
-      const extras =
-        (metaSpats as unknown[])
-          .map?.((v) => boundString(serialize(v), LOGGER_CONSTANTS.BOUNDARY_META_FILE))
-          .join(LOGGER_CONSTANTS.META_SPLIT) || '';
-
-      return [
-        boundString(timestamp as string, LOGGER_CONSTANTS.BOUNDARY_META_FILE),
-        extras ? `${LOGGER_CONSTANTS.META_SPLIT}${extras}` : '',
-        ':\n',
-        serialize(deserialize(message as string), 2),
-      ].join('');
-    });
-  }
-
-  private get fileTransports() {
-    const fileTransports = Object.values(ELoggerLevel).map(
-      (level) =>
-        new winston.transports.File({
-          filename: this.getLogFilePath(level),
-          level: level.toLowerCase(),
-          format: this.formatFile,
-        })
-    );
-
-    return fileTransports;
+    return [
+      boundString(timestamp as string, LOGGER_CONSTANTS.BOUNDARY_META_FILE),
+      extras ? `${LOGGER_CONSTANTS.META_SPLIT}${extras}` : '',
+      ':\n',
+      serialize(deserialize(message as string), 2),
+    ].join('');
   }
 
   private getLogFilePath(level: ELoggerLevel) {
     const logTimestamp = dayjs();
 
-    return path.join(
+    const filePath = path.join(
       logDir,
       bindStringFormat(LOGGER_CONSTANTS.FORMAT_FILES, {
         level: level.toString().toLowerCase(),
@@ -155,6 +124,50 @@ export class Log {
         year: logTimestamp.year(),
       })
     );
+
+    const dir = path.dirname(filePath);
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+
+    return filePath;
+  }
+
+  private writeLogFile(level: ELoggerLevel, message: string, ...meta: any[]) {
+    const filePath = this.getLogFilePath(level);
+
+    fs.writeFileSync(
+      filePath,
+      this.formatFile({
+        message,
+        level,
+        timestamp: dayjs().format(LOGGER_CONSTANTS.FORMAT_TIMESTAMP),
+        [Symbol.for('splat')]: meta,
+      }) + '\n',
+      { flag: 'a' }
+    );
+  }
+
+  private getLevel(level: ELoggerLevel) {
+    return level.toLowerCase();
+  }
+
+  private getLevelColor(level: string, boundChars: string[]) {
+    const color = {
+      [this.getLevel(ELoggerLevel.INFO)]: textColor.cyanBright.bold,
+      [this.getLevel(ELoggerLevel.WARN)]: textColor.yellowBright.bold,
+      [this.getLevel(ELoggerLevel.ERROR)]: textColor.redBright.bold,
+      [this.getLevel(ELoggerLevel.DEBUG)]: textColor.magentaBright.bold,
+    };
+
+    return (color[level as ELoggerLevel] || textColor.white)(boundString(level, boundChars));
+  }
+
+  private get customLevels() {
+    return {
+      [this.getLevel(ELoggerLevel.DEBUG)]: 0,
+      [this.getLevel(ELoggerLevel.INFO)]: 1,
+      [this.getLevel(ELoggerLevel.WARN)]: 2,
+      [this.getLevel(ELoggerLevel.ERROR)]: 3,
+    };
   }
 }
 
